a294d9db34244e7073b668f2ddd40882a519a668
gustavoadl06 <71989455+gustavoadl06@users.noreply.github.com>
2020-09-28 21:56:00 -0300

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,29 @@
+queue = []
+
+queue.append(int(input("1º valor da Fila: ")))
+queue.append(int(input("2º valor da Fila: ")))
+queue.append(int(input("3º valor da Fila: ")))
+queue.append(int(input("4º valor da Fila: ")))
+queue.append(int(input("5º valor da Fila: ")))
+queue.append(int(input("6º valor da Fila: ")))
+queue.append(int(input("7º valor da Fila: ")))
+queue.append(int(input("8º valor da Fila: ")))
+queue.append(int(input("9º valor da Fila: ")))
+queue.append(int(input("10º valor da Fila: ")))
+
+print(queue) # Imprime os valores colocados na fila
+
+
+#Começa a retirada. Dessa vez achei melhor ir retirando valor por valor , visto q na questão
+#anterior eu n fiz desa forma.
+queue.pop();    print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+(queue.pop());  print(queue)
+
