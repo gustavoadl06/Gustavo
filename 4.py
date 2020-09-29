3d5a3faea0103c2748b77ffa074ddbac1c9dc3f9
gustavoadl06 <71989455+gustavoadl06@users.noreply.github.com>
2020-09-28 21:33:06 -0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,30 @@
+pilha = []
+
+pilha.append(int(input("1º valor para ser colocado na pilha: ")))
+pilha.append(int(input("2º valor para ser colocado na pilha: ")))
+pilha.append(int(input("3º valor para ser colocado na pilha: ")))
+pilha.append(int(input("4º valor para ser colocado na pilha: ")))
+pilha.append(int(input("5º valor para ser colocado na pilha: ")))
+pilha.append(int(input("6º valor para ser colocado na pilha: ")))
+pilha.append(int(input("7º valor para ser colocado na pilha: ")))
+pilha.append(int(input("8º valor para ser colocado na pilha: ")))
+pilha.append(int(input("9º valor para ser colocado na pilha: ")))
+pilha.append(int(input("10º valor para ser colocado na pilha: ")))
+
+print(pilha)
+
+#Não tinha certeza se era para imprimir Cada retirada da pilha, mas como ficaria repetitivo, decidi n fazer.
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+pilha.pop()
+
+
+print(pilha)
+
